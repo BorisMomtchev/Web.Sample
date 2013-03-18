acc1df18a236546acaf28d0c53c1774888317b8f
Matthew Krizanac <matthewkrizanac@gmail.com>
2013-03-18 02:16:39 -0500

    Checking in missing files

---
+++ Web.Sample/Controllers/BootstrapExController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Sample.Controllers
+{
+    public class BootstrapExController : Controller
+    {
+        //
+        // GET: /BootstrapEx/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Buttons()
+        {
+            return View();
+        }
+
+        public ActionResult Collapsible()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web.Sample/Controllers/DropzoneExController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Sample.Controllers
+{
+    public class DropzoneExController : Controller
+    {
+        //
+        // GET: /DropzoneEx/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dropzone()
+        {
+            return View();
+        }
+
+    }
+}
