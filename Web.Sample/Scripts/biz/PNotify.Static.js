9ef2ff551ffdc1e78f6b30b9e53d12c5ceb53157
Matthew Krizanac <matthewkrizanac@gmail.com>
2013-04-14 17:31:47 -0500

    Local changes

---
+++ Web.Sample/Scripts/biz/PNotify.Static.js
@@ -0,0 +1,15 @@
+﻿var PNotify = {};
+PNotify.Static = {};
+
+//Ensures that the history isnt displayed by default. 
+//You can still override this behavior by setting the history property in the notification to true.
+pnotify.defaults.history = false;
+
+PNotify.Static.init = function() {
+    $("#btn_Simple").click(function() {
+        $.pnotify({
+            title: 'Regular Notice',
+            text: 'Check me out! I\'m a notice.'
+        });
+    });
+}
